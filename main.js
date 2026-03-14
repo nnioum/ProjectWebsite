abbdfaee707a5d013ee63ed5d35b26028bf81bd7
Serafima <simapono.4@gmail.com>
2026-03-14 08:25:19 +0700

    add

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 import { initDragDrop } from './dragDrop.js';
 import { initExecutor } from './executor.js';
 
-try {
-    await import('./error/ValidationError.js');
-    await import('./error/RuntimeError.js');
-} catch (e) {
-    console.error("Ошибка:", e);
-}
-
 const workspace = document.getElementById('workspace');
 const runBtn = document.getElementById('run-btn');
 const clearBtn = document.getElementById('clear-btn');
